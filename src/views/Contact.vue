80123c7f4b50bd377af86a3daf5d4a31d939bf8c
Ahmad-Qerem <alqerema@gmail.com>
2023-02-09 14:23:24 +0200

    chapter 5

---
+++ src/views/Contact.vue
@@ -0,0 +1,26 @@
+
+
+
+
+<template>
+    <div class="MainWraper">
+        <div>  Hello World</div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .MainWraper{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        border: 1px solid black;
+    }
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+</script>
+
+<template>
+  <main>
+    <div>
+      This Is Home Page
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    border: 1px solid black;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
